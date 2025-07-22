914a8da251278b5c658c7c79c794a2ae1e45b66e
Djordje3002 <djkartaljevic@gmail.com>
2025-07-22 20:54:47 +0200

    Set Up MVVM Structure and EnviromentObject

---
+++ MapAppSwift/ViewModel/LocationsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  LocationViewModel.swift
+//  MapAppSwift
+//
+//  Created by Djordje on 22. 7. 2025..
+//
+
+import SwiftUI
+
+struct LocationsViewModel: ObservableObject {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsViewModel()
+}
