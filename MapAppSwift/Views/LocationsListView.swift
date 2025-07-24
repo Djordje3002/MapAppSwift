c43e5f9121b54b608fd1ffefc21c9d0fb82a4290
Djordje3002 <djkartaljevic@gmail.com>
2025-07-24 17:51:10 +0200

    Locations list with places

---
+++ MapAppSwift/Views/LocationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsListView.swift
+//  MapAppSwift
+//
+//  Created by Djordje on 24. 7. 2025..
+//
+
+import SwiftUI
+
+struct LocationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsListView()
+}
