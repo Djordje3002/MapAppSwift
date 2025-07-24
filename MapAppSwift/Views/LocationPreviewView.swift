02189d73f8bdbad310f8eb07dd88355593d7577d
Djordje3002 <djkartaljevic@gmail.com>
2025-07-24 21:13:14 +0200

    LocationPreviewViewUI

---
+++ MapAppSwift/Views/LocationPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationPreviewView.swift
+//  MapAppSwift
+//
+//  Created by Djordje on 24. 7. 2025..
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationPreviewView()
+}
