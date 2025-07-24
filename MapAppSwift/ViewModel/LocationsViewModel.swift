68e9ee13b7e6135f805989455a94204d0a72f07b
Djordje3002 <djkartaljevic@gmail.com>
2025-07-24 16:43:38 +0200

    Update Map Region

--- MapAppSwift/ViewModel/LocationsViewModel.swift
+++ MapAppSwift/ViewModel/LocationsViewModel.swift
@@ -1,18 +1,31 @@
-//
-//  LocationViewModel.swift
-//  MapAppSwift
-//
-//  Created by Djordje on 22. 7. 2025..
-//
-
 import SwiftUI
+import MapKit
 
 class LocationsViewModel: ObservableObject {
     @Published var locations: [Location]
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    @Published var mapRegion: MKCoordinateRegion
+    
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
+        let firstLocation = locations.first!
+        
         self.locations = locations
+        self.mapLocation = firstLocation
+        self.mapRegion = MKCoordinateRegion(center: firstLocation.coordinates, span: mapSpan) // ✅ initialize here
+        updateMapRegion(location: firstLocation)
     }
     
+    private func updateMapRegion(location: Location) {
+        withAnimation {
+            mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapSpan)
+        }
+    }
 }
+
