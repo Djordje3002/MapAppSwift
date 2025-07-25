8a4770ebff2d4ff337bc1b062ecaf2c91eae513a
Djordje3002 <djkartaljevic@gmail.com>
2025-07-25 16:50:04 +0200

    Custom Anottation on map

---
+++ MapAppSwift/Views/AnotationView.swift
@@ -0,0 +1,18 @@
+//
+//  AnotationView.swift
+//  MapAppSwift
+//
+//  Created by Djordje on 25. 7. 2025..
+//
+
+import SwiftUI
+
+struct AnotationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnotationView()
+}
