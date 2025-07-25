64d9a781b9a3e2bdeb7571e7b13fca4a41eff34d
Djordje3002 <djkartaljevic@gmail.com>
2025-07-25 23:28:37 +0200

    Fixing resposibility for IPad

--- MapAppSwift/Views/LocationView.swift
+++ MapAppSwift/Views/LocationView.swift
@@ -15,6 +15,7 @@ struct LocationView: View {
         center: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922),
         span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     )
+    let maxWidthForIpad: CGFloat = 700
     
     var body: some View {
         ZStack {
@@ -22,6 +23,7 @@ struct LocationView: View {
             VStack(spacing: 0) {
                 header
                     .padding(.horizontal)
+                    .frame(maxWidth: maxWidthForIpad)
                 Spacer()
                 locationsPreviewStack
             }
@@ -91,6 +93,8 @@ extension LocationView {
                     LocationPreviewView(location: location)
                         .shadow(color: Color.black.opacity(0.3), radius: 20)
                         .padding()
+                        .frame(maxWidth: maxWidthForIpad)
+                        .frame(maxWidth: .infinity)
                         .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
                 }
             }
